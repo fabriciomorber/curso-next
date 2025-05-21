8cf49d9b273701cf4b11004fc00c729f67451230
Fabricio <fabmorber@gmail.com>
2025-05-21 15:16:51 -0300

    Salvando usuario no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
+import { supabase } from '@/src/services/supabase'
 
 const clientId = process.env.GITHUB_CLIENT_ID
 const clientSecret = process.env.GITHUB_CLIENT_SECRET
@@ -19,5 +20,31 @@ export default NextAuth({
         }
       }
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user
+
+      if (!email) return false
+
+      const {data, error} = await supabase
+        .from('users')
+        .select('id')
+        .eq('email', email)
+        .maybeSingle()
+
+      if (!data) {
+        const { error: insertError } = await supabase
+          .from('users')
+          .insert([{ email }])
+
+        if (insertError) {
+          console.error('Erro ao inserir usuário no Supabase:', insertError)
+          return false
+        }
+      }
+
+      return true
+    }
+  }
 })
