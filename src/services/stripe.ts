142418dde678e695769c8d4e96b6c8e11d961e6e
Fabricio <fabmorber@gmail.com>
2025-05-21 11:42:43 -0300

    Autenticação com Next Auth

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,7 +1,13 @@
 import Stripe from "stripe"
 
+const stripeKey = process.env.STRIPE_API_KEY
+
+if (!stripeKey) {
+  throw new Error("Missing GitHub OAuth credentials in environment variables")
+}
+
 export const stripe = new Stripe(
-  process.env.STRIPE_API_KEY,
+  stripeKey,
   {
     apiVersion: '2020-08-27',
     appInfo: {
