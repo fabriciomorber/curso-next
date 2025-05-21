7aec006dd3262e130e9e283715242879aa19bd72
Fabricio <fabmorber@gmail.com>
2025-05-21 10:49:22 -0300

    Parametrização nas rotas

---
+++ src/pages/api/users/[...params].tsx
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  console.log(request.query)
+
+  const users = [
+    { id: 1, name: 'Fabricio' },
+    { id: 2, name: 'Marisa' },
+    { id: 3, name: 'Philippe' },
+    { id: 4, name: 'Gustavo' },
+  ]
+
+  return response.json(users)
+}
